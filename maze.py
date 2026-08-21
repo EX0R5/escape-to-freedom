d5918d7faec87ad3d69b677d641b1b3895ad4b18
brianlvk09 <brianlevk@gmail.com>
2026-08-21 10:09:44 +0800

    Change attributes to private in maze and Room classes
    Updated attribute names in maze and Room classes to be private.

--- maze.py
+++ maze.py
@@ -1,15 +1,15 @@
 class maze:
   def __init__(self, rooms: List[Room], start_room: Room, monster: Monster):
-    self.rooms = rooms
-    self.start_room = start_room
-    self.monster = monster
+    self._rooms = rooms
+    self._start_room = start_room
+    self._monster = monster
 
 class Room:
   def __init__(self, name: str, description: str, sus_on_path: int, exits: Dict[str, Room],
                bosses: List[Boss], items: List[Items]):
-    self.name = name
-    self.description = description
-    self.sus_on_path = sus_on_path
-    self.exits = exits
-    self.bosses = bosses
-    self.items = items    
+    self._name = name
+    self._description = description
+    self._sus_on_path = sus_on_path
+    self._exits = exits
+    self._bosses = bosses
+    self._items = items    
