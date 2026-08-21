d2aedbbe5d73e48759f594a0481208d05e34afa8
EX0R5 <hisenochtang@gmail.com>
2026-08-21 10:01:17 +0800

    Enhance game loop with option display
    Refactor game loop to display options before choosing.

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     mud = game.Game()
     mud.welcome()
     while not mud.is_gameover():
-        choices = mud.get_options()
-        decision = mud.choose_option()
-        mud.execute(decision)
+        choices = mud.get_options()  # retrieve options as List[str]
+        mud.display_options(choices)  # display
+        decision = mud.choose_option()  # get them to choose
+        mud.execute_option(decision)  # execute given the str version of choice
     mud.epilogue()
     
