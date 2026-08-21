d5415aad173000bed182e023fcfbfa092e548bf8
brianlvk09 <brianlevk@gmail.com>
2026-08-21 10:06:33 +0800

    Change attributes to private in Dialogue class

--- dialogue.py
+++ dialogue.py
@@ -1,6 +1,6 @@
 class Dialogue:
   def __init__(self, display_text: str, gift_item: Item, added_sus: int, options: List[Dialogue]):
-    self.display_text = display_text
-    self.gift_item = gift_item
-    self.added_sus = added_sus
-    self.options = options
+    self._display_text = display_text
+    self._gift_item = gift_item
+    self._added_sus = added_sus
+    self._options = options
