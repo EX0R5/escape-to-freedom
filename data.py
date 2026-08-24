1c711e915dfd1bb8573c92942d5257627c7e743d
EX0R5 <hisenochtang@gmail.com>
2026-08-24 08:55:12 +0800

    Implement File class for file uploads
    Added a File class for handling file uploads, including methods to get the file name and read data from the file.

---
+++ data.py
@@ -0,0 +1,25 @@
+import csv
+
+class File:
+  """
+  Defines the uploading of a file onto the server.
+
+  Attributes:
+  _name (str): the name of the file
+  _data (List[Dict]): the data in the file
+  """
+  def __init__(self, name) -> None:
+    self._name = name
+    self._data = None
+
+  def get_name(self) -> str:
+    return self._name
+
+  def read(self) -> None:
+    data = []
+    with open(self.get_name()) as f:
+      reader = csv.reader()
+      for row in reader:
+        data.append(row)
+
+    self._data = data
