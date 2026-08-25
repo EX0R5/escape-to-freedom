5dae3c43d32b74bf070bc3b8e19d80463b3f9894
EX0R5 <hisenochtang@gmail.com>
2026-08-25 12:27:13 +0800

    Rename maze class to Maze and add docstring
    Refactor maze class to Maze with improved docstring.

--- maze.py
+++ maze.py
@@ -1,8 +1,16 @@
-class maze:
-  def __init__(self, rooms: List[Room], start_room: Room, monster: Monster):
-    self._rooms = rooms
-    self._start_room = start_room
-    self._monster = monster
+class Maze:
+    def __init__(self, rooms: List[Room], start_room: Room, monster: Monster):
+        """
+        Defines a Maze.
+
+        Attributes:
+        rooms (List[Room]): rooms in the maze
+        start_room (Room): starting room
+        monster (Monster): the monster in the maze
+        """
+        self._rooms = rooms
+        self._start_room = start_room
+        self._monster = monster
 
 class Room:
   def __init__(self, name: str, description: str, sus_on_path: int, exits: Dict[str, Room],
